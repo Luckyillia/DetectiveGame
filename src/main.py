00b629b05c4d36962bdeea853290e68eba7c83c5
lcukyillia <zajchenkoi2007@gmail.com>
2025-05-07 00:41:46 +0200

    Delete image and reset privious state

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import os
+
 from nicegui import app, ui
 from src.services.login import AuthMiddleware
 from src.ui.user_ui import UserUI
@@ -14,15 +16,4 @@ if __name__ in {"__main__", "__mp_main__"}:
 def main_page() -> None:
     UserUI()
 
-ui.add_body_html("""
-<style>
-body {
-    background-image: url('https://i.imgur.com/6jHnBoh.jpeg');
-    background-size: cover;
-    background-attachment: fixed;
-    background-position: center;
-    background-repeat: no-repeat;
-}
-</style>
-""")
 ui.run(storage_secret='natka.zajk79', on_air='Exb5blKCa7JaTEOd', port=1234)
